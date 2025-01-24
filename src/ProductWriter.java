7718d75b584d57d55ed441934f950f7185f3965e
krishsah01 <157822205+krishsah01@users.noreply.github.com>
2025-01-24 15:35:07 -0500

    Product Reader

--- src/ProductWriter.java
+++ src/ProductWriter.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.Scanner;import java.io.BufferedOutputStream;
+import java.util.Scanner;
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -11,62 +12,52 @@ import java.nio.file.Paths;
 import static java.nio.file.StandardOpenOption.*;
 
 public class ProductWriter {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         boolean doneInput = false;
         String ID = "";
-        String firstName = "";
-        String lastName = "";
-        String title = "";
+        String Name = "";
+        String Description = "";
         String rec = "";
-        int YOB = 0;
+        Double Cost = 0.0;
 
-        ArrayList<String> people = new ArrayList<String>();
+        ArrayList<String> goods = new ArrayList<String>();
         Scanner in = new Scanner(System.in);
 
-        // create a loop to enter persons data
+        // create a loop to enter products data
         do {
             ID = SafeInput.getNonZeroLenString(in, "Enter ID: ");
-            firstName = SafeInput.getNonZeroLenString(in, "Enter First Name: ");
-            lastName = SafeInput.getNonZeroLenString(in, "Enter Last Name: ");
-            title = SafeInput.getNonZeroLenString(in, "Enter Title: ");
-            YOB = SafeInput.getRangedInt(in, "Enter Year of Birth: ", 1000, 2025);
-            rec = ID + ", " + firstName + ", " + lastName + ", " + title + ", " + YOB;
+            Name = SafeInput.getNonZeroLenString(in, "Enter Name: ");
+            Description = SafeInput.getNonZeroLenString(in, "Enter Description: ");
+            Cost = SafeInput.getRangedDouble(in, "Enter Cost: ", 0, 9999999);
+            rec = ID + ", " + Name + ", " + Description + ", " + Cost;
 
             System.out.println(rec);
 
-            people.add(rec);
+            goods.add(rec);
 
-            doneInput = SafeInput.getYNConfirm(in, "Are you done entering people ");
+            doneInput = SafeInput.getYNConfirm(in, "Are you done entering goods?");
 
-        }while(!doneInput);
+        } while (!doneInput);
+
         File workingDirectory = new File(System.getProperty("user.dir"));
-        Path file = Paths.get(workingDirectory.getPath(), "src", "PersonTestData.txt");
+        String fileName = "ProductTestData" + ".txt";
+        Path file = Paths.get(workingDirectory.getPath(), "src", fileName);
 
-        try
-        {
+        try {
             // Typical java pattern of inherited classes
             // we wrap a BufferedWriter around a lower level BufferedOutputStream
-            OutputStream out =
-                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
-            BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(out));
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
 
-            // Finally can write the file LOL!
-
-            for(String person : people)
-            {
-                writer.write(person, 0, person.length());  // stupid syntax for write rec
-                // 0 is where to start (1st char) the write
-                // rec. length() is how many chars to write (all)
+            // Write all records to the file
+            for (String product : goods) {
+                writer.write(product, 0, product.length());
                 writer.newLine();  // adds the new line
-
             }
+
             writer.close(); // must close the file to seal it and flush buffer
-            System.out.println("Data file written!");
-        }
-        catch (IOException e)
-        {
+            System.out.println("Data file " + fileName + " written!");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
